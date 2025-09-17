1938cc899db1983b93c99348c7e0f3ddd455fade
Leeonejae6708 <leewj6708@gmail.com>
2025-09-17 16:03:49 +0900

    Time: 387 ms (57.95%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+select 
+    x, 
+    y, 
+    z, 
+    case
+        when x + y > z and y + z > x and z + x > y then 'Yes'
+        else 'No'
+    end triangle
+from Triangle;
